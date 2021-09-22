a180babe7fbb88db89e8a26c994eaf9f8065bcbf
Edidiong Esu <eddy.esu.22@gmail.com>
2021-09-22 15:15:29 +0100

    First commit

--- main.dart
+++ main.dart
@@ -1,7 +1,13 @@
-import 'dart:io';
+void person(username, age) {
+  print("My name is $username and I am $age years");
+}
 
 void main() {
-  /*print("Input your first number:");
+  /*
+  import 'dart:io';
+  DAta types:
+
+  print("Input your first number:");
   var first = int.parse(stdin.readLineSync()!); // the ! sign is for null safety
   print("Input your second number:");
   var second = int.parse(stdin.readLineSync()!);
@@ -10,18 +16,31 @@ void main() {
 
   print("The sum of the numbers is: $summer ");
 
+  -------Lists------------
   List favNum = [first, second];
 
   favNum.add(64);
 
   print(favNum);
 
-  */
-
   // indexOf, contains, remove, add
 
   List freshList = [3, 5, 5, 5, 6, 7, 8];
   freshList.remove(5);
-
   print(freshList);
+
+
+  ------Functions-----------------------
+  void drawTriangle() {
+  print(" /|");
+  print("/_|");
+
+  drawTriangle();
+
+  //Void can not return any value.
+}
+}
+   */
+
+  person(54, 34);
 }
